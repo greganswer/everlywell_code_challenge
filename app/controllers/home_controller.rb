cd93ae7db03118aefca08cf4b26170b611d46ed9
Greg Answer <greganswer@gmail.com>
2020-03-27 17:35:43 -0400

    Add root URL for test purposes

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# Non resource routes.
+class HomeController < ApplicationController
+  def index
+    render json: {
+      name: 'Experts Directory Search Tool',
+      description: 'Search for experts based on headings on their personal website',
+      version: '0.1.0'
+    }
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root to: 'home#index'
+
   resources :users, only: %i[index show create]
 end
